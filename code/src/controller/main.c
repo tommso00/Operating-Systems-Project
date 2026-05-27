933909596706b75b3b895b3f10e6bd23d173f5b2
tommso00 <tommaso.santus@studenti.unitn.it>
2026-05-27 19:34:33 +0000

    Aggiornti main.c e commands.c

--- code/src/controller/main.c
+++ code/src/controller/main.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "controller.h"
 #include "error_codes.h"
 
-int bulb_device_main(device_id id);
-
-int main(int argc, char **argv) {
-    controller controller;
+int main(void){
+    controller ctrl;
     int rc;
+    int cleanup_rc;
 
-    if (argc == 3 && strcmp(argv[1], "--device-bulb") == 0) {
-        device_id id = (device_id)atoi(argv[2]);
-        return bulb_device_main(id);
-    }
-
-    rc = controller_init(&controller);
-    if (rc != OK) {
-        fprintf(stderr, "controller_init failed: %s\n", error_str(rc));
+    rc= controller_init(&ctrl);
+    if(rc!=OK){
+        fprintf(stderr, "controller_init feiled with code %d\n",rc);
         return rc;
     }
 
-    rc = controller_run(&controller);
-    if (rc != OK) {
-        fprintf(stderr, "controller_run failed: %s\n", error_str(rc));
+    rc = controller_run(&ctrl);
+    if(rc!=OK){
+        fprintf(stderr, "controller_run failed with code %d\n", rc);
     }
 
-    {
-        int cleanup_rc = controller_destroy(&controller);
-        if (cleanup_rc != OK && rc == OK) {
+    cleanup_rc =  controller_destroy(&ctrl);
+    if(cleanup_rc!=OK){
+        fprintf(stderr, "controller_destroy failed with code %d\n", cleanup_rc);
+        if(rc==OK){
             rc = cleanup_rc;
         }
     }
